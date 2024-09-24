bd96ef3f4082859102d847f0917d17c0142d8c60
Jakub <derivatives_four@protonmail.com>
2024-09-24 18:48:17 +0200

    feat: add password reset related controllers
    - Added `requestPasswordReset` and `passwordReset` functions to `authController` which are used to reset the password.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,17 @@
 // import dependencies
 const userModel = require('../models/userModel')
+const pwTokenModel = require('../models/pwTokenModel')
+const CustomError = require('../classes/customError')
+const crypto = require('crypto')
+const sendEmail = require('../utils/sendEmail')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 const {
-  NODE_ENV,
+  production,
+  ORIGIN,
   ACCESS_TOKEN_SECRET, 
-  REFRESH_TOKEN_SECRET
+  REFRESH_TOKEN_SECRET,
 } = require('../config/env')
-
-const production = NODE_ENV === 'prod'
 
 const signup = async (req, res, next) => {
   try {
@@ -195,9 +199,97 @@ const refreshToken = async (req, res) => {
   )
 }
 
+const requestPasswordReset = async (req, res, next) => {
+  try {
+    const {email} = req.body
+  
+    const user = await userModel.findOne({email})
+  
+    if (!user) throw new CustomError('Email not registered', 400)
+  
+    let token = await pwTokenModel.findOne({userId: user._id}) // check if user has a token
+  
+    if (token) await token.deleteOne() // if token exists delete it
+  
+    let resetToken = crypto.randomBytes(32).toString('hex') // create reset token using Node.js crypto API
+  
+    // hash resetToken
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(resetToken, salt)
+  
+    // save hashed resetToken in DB
+    await pwTokenModel.create({
+      userId: user._id,
+      token: hash,
+      createdAt: Date.now()
+    })
+
+    const link = `${ORIGIN}/password-reset?token=${resetToken}&uuid=${user.uuid}` // define reset password link containing reset token and user uuid
+    sendEmail(
+      user.email, // email
+      'Password Reset Request', // subject
+      {
+        name: user.username[0].toUpperCase() + user.username.slice(1), 
+        link: link
+      }, // payload
+      './template/requestPasswordReset.handlebars', // handlebars template
+      res, // pass res object
+      next // past next fn
+    )
+  } catch (error) {
+    next(error)
+  }
+}
+
+const passwordReset = async (req, res, next) => {
+  try {
+    const {uuid, token, password} = req.body
+
+    const user = await userModel.findOne({uuid: uuid}) // find user by uuid
+
+    if (!user) throw new CustomError('User not found', 404)
+
+    const pwResetToken = await pwTokenModel.findOne({userId: user._id}) // find pw reset token by user _id
+    
+    if (!pwResetToken) throw new CustomError('Please request a new password reset link', 404)
+
+    const isValid = await bcrypt.compare(token, pwResetToken.token) // compare FE token with DB token
+
+    if (!isValid) throw new CustomError('Please request a new password reset link', 400)
+
+    // hash new password
+    const salt = await bcrypt.genSalt(10)
+    const pwHash = await bcrypt.hash(password, salt)
+
+    // update password in the user document
+    await userModel.findByIdAndUpdate(
+      user._id,
+      {$set: {password: pwHash}},
+      {new: true}
+    )
+
+    sendEmail(
+      user.email, // email
+      'Password Reset Successful', // subject
+      {
+        name: user.username[0].toUpperCase() + user.username.slice(1)
+      }, // payload
+      './template/passwordReset.handlebars', // handlebars template
+      res, 
+      next
+    )
+    
+    await pwResetToken.deleteOne() // delete the password reset token when operation is successful
+  } catch (error) {
+    next(error)
+  }
+}
+
 module.exports = {
   signup,
   login,
   logout,
-  refreshToken
+  refreshToken,
+  requestPasswordReset,
+  passwordReset
 }
