f2dadc5fdd970bb9832ffff632d3636efac8e219
Jakub <derivatives_four@protonmail.com>
2024-09-24 18:49:53 +0200

    refactor: add `if` statement to `errorHandlerMiddleware`

--- middleware/errorHandler/errorHandlerMiddleware.js
+++ middleware/errorHandler/errorHandlerMiddleware.js
@@ -2,12 +2,14 @@
 const CustomError = require("../../classes/customError")
 
 const errorHandler = (err, req, res, next) => {
-  console.log('\nerrorHandler: ', err)
-  console.log('\nerrorStack', err.stack)
+  if (err) {
+    console.log('\nerrorHandler: ', err)
+    console.log('\nerrorStack', err.stack)
 
-  // check if error is a known error
-  if (err instanceof CustomError) {
-    return res.status(err.code).json({error: err.message})
+    // check if error is a known error
+    if (err instanceof CustomError) {
+      return res.status(err.code).json({error: err.message})
+    }
   }
 
   return res.status(500).json({error: 'Unexpected error ocurred'})
