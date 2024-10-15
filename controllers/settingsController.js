87385d8bd7f18d18cd74f5e8dbd4d04678acd557
Jakub <derivatives_four@protonmail.com>
2024-10-15 10:38:03 +0200

    fix: add missing emojis
    - Added missing emojis to `errorHandlerMiddleware` and `settingsController`.

--- controllers/settingsController.js
+++ controllers/settingsController.js
@@ -10,7 +10,7 @@ const uploadAvatar = async (req, res, next) => {
 
     if (!file) throw new CustomError('Please select a file', 400)
 
-    console.log('\nUPLOAD AVATAR 🔍\nAvatar file: ', file)
+    console.log('\nUPLOAD AVATAR 🔍\n 👉 Avatar file: ', file)
 
     // find user
     const user = await userModel.findById(id)
--- middleware/errorHandler/errorHandlerMiddleware.js
+++ middleware/errorHandler/errorHandlerMiddleware.js
@@ -3,7 +3,7 @@ const CustomError = require("../../classes/customError")
 
 const errorHandler = (err, req, res, next) => {
   if (err) {
-    console.log(`\nERROR HANDLER 🔍\nError: ${err}\nStack: ${err.stack}`)
+    console.log(`\nERROR HANDLER 🔍\n 👉 Error: ${err}\n 👉 Stack: ${err.stack}`)
 
     // check if error is a known error
     if (err instanceof CustomError) {
