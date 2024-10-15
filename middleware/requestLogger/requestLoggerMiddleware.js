10153ce610ee0020e055fadbd5562567762d296e
Jakub <derivatives_four@protonmail.com>
2024-10-15 10:32:24 +0200

    fix: `requestLoggerMiddleware` separate app instance
    - Problem: The `requestLoggerMiddleware` used a separate Express app instance on top of the one declared in `server.js`.
    - Solution: Removed the redundant Express app instance, and exported only the middleware function in `requestLoggerMiddleware`.

--- middleware/requestLogger/requestLoggerMiddleware.js
+++ middleware/requestLogger/requestLoggerMiddleware.js
@@ -1,13 +1,7 @@
-// import dependencies
-const express = require('express')
-
-// deploy requestLogger
-const requestLogger = express()
-
-requestLogger.use((req, res, next) => {
+const requestLogger = (req, res, next) => {
   console.log(`\nREQUEST LOGGER 🔍\n 👉 Request path: ${req.path}\n 👉 Request method: ${req.method}\n 👉 Request body: ${JSON.stringify(req.body, null, 2)}\n 👉 Request query: ${JSON.stringify(req.query, null, 2)}\n 👉 Request cookie: ${req.headers.cookie}\n 👉 Request headers: ${JSON.stringify(req.headers, null, 2)}`)
   
   next()
-})
+}
 
 module.exports = requestLogger
